88f25fcd609527edbe887e9481c0c61ab55c2ae9
luvega-rj <luvegaronny@gmail.com>
2023-03-10 14:17:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names =["Ada","Brendan","Ali"];
+const x = "birthday";
+function writeCards(names, x){
+    let result = [];
+    for (let i=0; i<names.length; i++){
+        result.push(`Thank you, ${names[i]}, for the wonderful ${x} gift!`)
+        //debugger
+    }
+    return result;
+}
+
+function countDown(number){
+ while (number>=0){
+    console.log(number--)
+ }
+ }
+    countDown()
